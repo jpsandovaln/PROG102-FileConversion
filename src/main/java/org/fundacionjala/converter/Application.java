d0dff22e0751318a031c37086fd785d8cc455bee
laura helen montano toro <hlaura.toro@gmail.com>
2020-09-07 17:11:43 -0400

    Added File Service with methods to interact with database

--- src/main/java/org/fundacionjala/converter/Application.java
+++ src/main/java/org/fundacionjala/converter/Application.java
@@ -1,7 +1,7 @@
 package org.fundacionjala.converter;
 
 import org.fundacionjala.converter.model.entity.File;
-import org.fundacionjala.converter.model.repository.FileRepository;
+import org.fundacionjala.converter.model.service.FileService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,25 +18,23 @@ public class Application {
      * @return
      */
     @Bean
-    public CommandLineRunner fileDemo(final FileRepository fileRepository) {
+    public CommandLineRunner fileDemo(final FileService fileService) {
         return (args) -> {
 
         // create Files
-        fileRepository.save(new File("file1", "md51"));
-        fileRepository.save(new File("file2", "md52"));
-        fileRepository.save(new File("file3", "md53"));
+        /*fileService.saveFile(new File("file1", "md51"));
+        fileService.saveFile(new File("file2", "md52"));
+        fileService.saveFile(new File("file3", "md53"));*/
 
         // fetch all Files
         System.out.println("Files found with findAll():");
         System.out.println("---------------------------");
-        for (File file : fileRepository.findAll()) {
-            System.out.println(file.toString());
-        }
+        fileService.getFileList();
         System.out.println();
 
         // fetch File by id
-        File file = fileRepository.findById(1L).get();
-        System.out.println("File found with findById(1):");
+        File file = fileService.getFileById(2);
+        System.out.println("File found with findById(2):");
         System.out.println("-----------------------------");
         System.out.println(file.toString());
         System.out.println();
@@ -44,23 +42,15 @@ public class Application {
         // fetch all Files that contain keyword `md53`
         System.out.println("Files that contain keyword 'md53':");
         System.out.println("-----------------------------------");
-        File fileTemp = fileRepository.findByMd5("md53");
+        File fileTemp = fileService.getFileByMd5("md53");
         System.out.println(fileTemp.toString());
         System.out.println();
 
         // update File path
-        File fileUpdate = fileRepository.findById(2L).get();
-        fileUpdate.setPath("filePath2");
-        fileRepository.save(fileUpdate);
+        File fileUpdate = new File("filePath123", "md51");
         System.out.println("Update File path:");
         System.out.println("------------------");
-        System.out.println(fileUpdate.toString());
-        System.out.println();
-
-        // total Files in DB
-        System.out.println("Total Files in DB:");
-        System.out.println("------------------");
-        System.out.println(fileRepository.count());
+        fileService.updateFile(fileUpdate);
         System.out.println();
         };
     }
