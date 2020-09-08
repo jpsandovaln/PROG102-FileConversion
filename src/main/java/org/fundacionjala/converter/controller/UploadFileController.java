554bb1150555a4c730278f3831887efabfeea444
julia-jpg <julia.escalante@fundacion-jala.org>
2020-09-08 15:11:44 -0400

    solved checkstyle

--- src/main/java/org/fundacionjala/converter/controller/UploadFileController.java
+++ src/main/java/org/fundacionjala/converter/controller/UploadFileController.java
@@ -7,7 +7,12 @@ import org.fundacionjala.converter.model.service.FileUploadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
