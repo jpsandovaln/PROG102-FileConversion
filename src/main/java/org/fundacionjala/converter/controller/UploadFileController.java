66cfea519b5863f2bdea8403910cf5b2130b830b
julia-jpg <julia.escalante@fundacion-jala.org>
2020-09-08 10:32:03 -0400

    modified uploadFileController and merge Laura's branch

--- src/main/java/org/fundacionjala/converter/controller/UploadFileController.java
+++ src/main/java/org/fundacionjala/converter/controller/UploadFileController.java
@@ -1,23 +1,24 @@
 package org.fundacionjala.converter.controller;
 
 import org.fundacionjala.converter.model.ChecksumMD5;
+import org.fundacionjala.converter.model.entity.File;
+import org.fundacionjala.converter.model.service.FileService;
 import org.fundacionjala.converter.service.FileUploadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
+import java.util.List;
 
 @RestController
 public class UploadFileController {
 
     @Autowired
     private FileUploadService fileUploadService;
+    @Autowired
+    private FileService fileService;
 
     /**
      * displays upload file without param
@@ -34,22 +35,21 @@ public class UploadFileController {
      * @return file upload successfully with md5.
      * @throws Exception
      */
-    @PostMapping("upload")
+    @PostMapping("/upload")
     public ResponseEntity<?> uploadFile(@RequestParam("file") final MultipartFile file) throws Exception {
         if (file == null || file.isEmpty()) {
             return new ResponseEntity<Object>("Select a file", HttpStatus.OK);
         }
+        ChecksumMD5 checksumMD5 = new ChecksumMD5();
         String checksum = "";
-        try {
-            String filePath = fileUploadService.saveInputFile(file);
-            ChecksumMD5 checksumMD5 = new ChecksumMD5();
-            checksum = checksumMD5.getMD5(filePath);
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        String filePath = fileUploadService.saveInputFile(file);
+        checksum = checksumMD5.getMD5(filePath);
+        if (fileService.getFileByMd5(checksum) == null) {
+            fileService.saveFile(new File(filePath, checksum));
+            return new ResponseEntity<Object>("file upload successfully" + "\n" + checksum, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<Object>("the file already exists" + "\n" + checksum, HttpStatus.OK);
         }
-        return new ResponseEntity<Object>("file upload successfully" + "\n" + checksum, HttpStatus.OK);
     }
     /**
      * displays file upload successfully with message correct or incorrect md5
@@ -58,29 +58,82 @@ public class UploadFileController {
      * @return file upload successfully with message correct or incorrect md5
      * @throws Exception
      */
-    @PostMapping("upload-md5")
+    @PostMapping("/upload-md5")
     public ResponseEntity<?> uploadFile(@RequestParam("file") final MultipartFile file, @RequestParam final String md5) throws Exception {
         if (file == null || file.isEmpty()) {
             return new ResponseEntity<Object>("Select a file", HttpStatus.OK);
         }
-        //md5 verificar si hay bd si existe -> path de data base,
-        //sino validar
+        ChecksumMD5 checksumMD5 = new ChecksumMD5();
         String checksum = "";
-        try {
+        boolean correctMd5;
+        if (fileService.getFileByMd5(md5) == null) { //no existe
             String filePath = fileUploadService.saveInputFile(file);
-            ChecksumMD5 checksumMD5 = new ChecksumMD5();
             checksum = checksumMD5.getMD5(filePath);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String message = "";
-        //checksum.equals(md5);
-        if (checksum.equals(md5)) {
-            message = "correct MD5";
-            //data-base
+            correctMd5 = checksum.equals(md5);
+            fileService.saveFile(new File(filePath, checksum));
+            return new ResponseEntity<Object>("file upload successfully" + "\n" + "checksum entered is" + correctMd5, HttpStatus.OK);
         } else {
-            message = "Incorrect MD5";
+            String filePath = fileService.getFileByMd5(md5).getPath();
+            checksum = checksumMD5.getMD5(filePath);
+            correctMd5 = checksum.equals(md5);
+            return new ResponseEntity<Object>("the file already exists" + "\n" + "checksum entered is: " + correctMd5, HttpStatus.OK);
         }
-        return new ResponseEntity<Object>("file upload successfully" + "\n" + message + "\n" + checksum, HttpStatus.OK);
+    }
+
+    /**
+     * delete the file in data base
+     * @param file to delete
+     */
+    @DeleteMapping("/file")
+    public void addFile(@RequestParam final File file) {
+        fileService.deleteFile(file);
+    }
+
+    /**
+     * delete the file according to your md5
+     * @param md5's file to delete
+     */
+    @DeleteMapping("/file{md5}")
+    public void addFile(@RequestParam final String md5) {
+        fileService.deleteFileByMd5(md5);
+    }
+
+    /**
+     * get List files of database
+     * @return list files
+     */
+    @GetMapping("/files")
+    public List<File> getFiles() {
+        return fileService.getFileList();
+    }
+
+    /**
+     * get file with id
+     * @param id to find
+     * @return file
+     */
+
+    @GetMapping("/file{id}")
+    public File getFile(@RequestParam final int id) {
+        return  fileService.getFileById(id);
+    }
+
+    /**
+     * get File with md5
+     * @param md5 to find
+     * @return file
+     */
+    @GetMapping("/file{md5}")
+    public File getFile(@RequestParam final String md5) {
+        return  fileService.getFileByMd5(md5);
+    }
+
+    /**
+     * update file in data base
+     * @param file to update
+     */
+    @PostMapping("/file")
+    public void updateFile(@RequestParam final File file) {
+        fileService.updateFile(file);
     }
 }
