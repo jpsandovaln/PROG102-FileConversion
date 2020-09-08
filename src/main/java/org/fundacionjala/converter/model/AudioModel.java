1963f46a13aebcae140c389c44f6ffaf9dfbfd9f
Mirko Romay Ramos <mirko.romay@fundacion-jala.org>
2020-09-08 16:08:55 -0400

    Refactor AudioControler methods and adding AudioParameter class usage

--- src/main/java/org/fundacionjala/converter/model/AudioModel.java
+++ src/main/java/org/fundacionjala/converter/model/AudioModel.java
@@ -1,67 +1,24 @@
 package org.fundacionjala.converter.model;
-
-import org.springframework.web.multipart.MultipartFile;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 
 public class AudioModel {
 
+    private String buildCommand(final String params, final String source, final String target, final String tool) {
+        return tool + " -y -i "  + source + params  + target;
+    }
+
     /**
      * Show all output of audio file conversion
-     * @param file uploaded
-     * @param format string
-     * @param codec string
-     * @param bitRate string
-     * @param channels string
-     * @param sampleRate string
-     * @param start string
-     * @param duration string
-     * @param vol string
-     * @return all output of audio file conversion
+     * @param params string
+     * @param source string
+     * @param target string
+     * @param tool string
+     * @return path of audio file converted
      */
-    public String convertAudio(final MultipartFile file, final String format, final String codec,
-                               final String bitRate, final String channels, final String sampleRate,
-                               final String start, final String duration, final String vol,
-                               final String input, final String converted, final String tool) {
+    public String convertAudio(final String params, final String source, final String target, final String tool)  {
         try {
-            String fileName = file.getOriginalFilename();
-            String exitThree = "";
-            String source = input + fileName;
-            String fileNameConverted = fileName.substring(0, fileName.indexOf("."));
-            String target = converted + fileNameConverted + "-converted" + format;
-            Files.copy(file.getInputStream(), Paths.get(source), StandardCopyOption.REPLACE_EXISTING);
-            source = exitThree + source;
-            target = exitThree + target;
-            String outputParameters = "";
-            if (codec != "") {
-                outputParameters += " -codec:a " + codec;
-            }
-            if (bitRate != "") {
-                outputParameters += " -b:a " + bitRate;
-            }
-            if (channels != "") {
-                outputParameters += " -ac " + channels;
-            }
-            if (sampleRate != "") {
-                outputParameters += " -ar " + sampleRate;
-            }
-            if (start != "") {
-                outputParameters += " -ss " + start;
-            }
-            if (duration != "") {
-                outputParameters += " -t " + duration;
-            }
-            if (vol != "") {
-                outputParameters += " -vol " + vol;
-            }
-            outputParameters += " ";
-            String projectDirectory = System.getProperty("user.dir") + "/";
-            projectDirectory = projectDirectory.replace("\\", "/");
-            String command = projectDirectory + tool + " -y -i " + projectDirectory + source + outputParameters + projectDirectory + target;
+            String command = buildCommand(params, source, target, tool);
             ProcessBuilder builder = new ProcessBuilder("cmd", "/c", "\"" + command + "\"");
             builder.redirectErrorStream(true);
             Process process = builder.start();
@@ -72,11 +29,14 @@ public class AudioModel {
             while (reader.ready()) {
                 result.append((char) reader.read());
             }
-            return result.toString();
+            System.out.println(result.toString());
+            return "Your converted audio is located at: " + target;
         } catch (java.io.IOException e) {
             return e.getMessage();
         } catch (java.lang.InterruptedException e) {
             return e.getMessage();
         }
     }
+
+
 }
