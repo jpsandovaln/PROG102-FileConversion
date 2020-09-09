53c12dcfcde9f5eaf901ef83c8e639a98f73f889
eli7890 <eliz.bf.4422@gmail.com>
2020-09-09 00:42:10 -0400

    view metadata

--- src/main/java/org/fundacionjala/converter/controller/HomeController.java
+++ src/main/java/org/fundacionjala/converter/controller/HomeController.java
@@ -2,13 +2,17 @@ package org.fundacionjala.converter.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
+/**
+ * @author Elizabeth Bravo
+ * @version 0.1
+ */
 @Controller
 public class HomeController {
+    /**
+    * displays home view
+    * @return home view
+    */
     @GetMapping(value = {"/","home"})
-    //@RequestMapping(value = {"/","home"})
     public String home() {
         return "home";
     }
