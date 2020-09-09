72c98014302090bdd351d2139f9278143a3c5cef
Mirko Romay Ramos <mirko.romay@fundacion-jala.org>
2020-09-09 12:08:02 -0400

    CheckStyle corrections

--- src/main/java/org/fundacionjala/converter/controller/ImageController.java
+++ src/main/java/org/fundacionjala/converter/controller/ImageController.java
@@ -1,25 +1,62 @@
 package org.fundacionjala.converter.controller;
 import org.fundacionjala.converter.model.ImageModel;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class ImageController {
 
+    /**
+     *
+     * @param checkSum
+     * @param file
+     * @param newName
+     * @param extension
+     * @param height
+     * @param weight
+     * @param thumbnail
+     * @param forceResize
+     * @return
+     */
     @RequestMapping(value = "/image/resize", method = RequestMethod.POST)
-    public String Convert(@RequestParam("checkSum") String checkSum, @RequestParam("file") MultipartFile file, @RequestParam("newName") String newName, @RequestParam("Extension") String extension, @RequestParam("height") int height, @RequestParam("weight") int weight, @RequestParam("thumbnail") int thumbnail ) {
+    public String convert(@RequestParam("file") final MultipartFile file, @RequestParam("newName") final String newName, @RequestParam("Extension") final String extension, @RequestParam("height") final int height, @RequestParam("weight") final int weight, @RequestParam("thumbnail") final int thumbnail, @RequestParam("forceResize") final int forceResize) {
         ImageModel img =  new ImageModel();
-        return img.execute(file, newName, height, weight, extension, thumbnail);
+        return img.execute(file, newName, height, weight, extension, thumbnail, forceResize);
     }
 
+    /**
+     *
+     * @param checkSum
+     * @param file
+     * @param newName
+     * @param extension
+     * @param height
+     * @param weight
+     * @param positionX
+     * @param positionY
+     * @return
+     */
     @RequestMapping(value = "/image/crop", method = RequestMethod.POST)
-    public String cropImage(@RequestParam("checkSum") String checkSum, @RequestParam("file") MultipartFile file, @RequestParam("newName") String newName, @RequestParam("Extension") String extension, @RequestParam("height") int height, @RequestParam("weight") int weight, @RequestParam("positionX")  int positionX, @RequestParam("positionY") int positionY) {
+    public String cropImage(@RequestParam("file") final MultipartFile file, @RequestParam("newName") final String newName, @RequestParam("Extension") final String extension, @RequestParam("height") final int height, @RequestParam("weight") final int weight, @RequestParam("positionX")  final int positionX, @RequestParam("positionY") final int positionY) {
         ImageModel img =  new ImageModel();
-        return img.selectingImageRegion(file,newName,height,weight,positionX,positionY, extension);
+        return img.selectingImageRegion(file, newName, height, weight, positionX, positionY, extension);
     }
 
+    /**
+     *
+     * @param checkSum
+     * @param file
+     * @param newName
+     * @param extension
+     * @param height
+     * @param weight
+     * @return
+     */
     @RequestMapping(value = "/image/gris_scale", method = RequestMethod.POST)
-    public String grisScaleImage(@RequestParam("checkSum") String checkSum, @RequestParam("file") MultipartFile file, @RequestParam("newName") String newName, @RequestParam("Extension") String extension, @RequestParam("height") int height, @RequestParam("weight") int weight) {
+    public String grisScaleImage(@RequestParam("checkSum") final String checkSum, @RequestParam("file") final MultipartFile file, @RequestParam("newName") final String newName, @RequestParam("Extension") final String extension, @RequestParam("height") final int height, @RequestParam("weight") final int weight) {
         ImageModel img =  new ImageModel();
         return img.grayScale(file, newName, extension);
     }
--- src/main/java/org/fundacionjala/converter/model/ImageModel.java
+++ src/main/java/org/fundacionjala/converter/model/ImageModel.java
@@ -7,64 +7,86 @@ import java.io.InputStreamReader;
 
 public class ImageModel {
 
-    public String execute(MultipartFile file , String newName , int height , int weight , String extension, int thumbnail, int forceResize) {
+    /**
+     *
+     * @param file
+     * @param newName
+     * @param height
+     * @param weight
+     * @param extension
+     * @param thumbnail
+     * @param forceResize
+     * @return
+     */
+    public String execute(final MultipartFile file, final String newName, final int height, final int weight, final String extension, final int thumbnail, final int forceResize) {
         try {
             String path = "C:\\Users\\Administrator\\Desktop";
             String fileName = file.getOriginalFilename();
             String force = "";
-            if(forceResize == 1) {
+            if (forceResize == 1) {
                 force = "!";
             }
             String cmd = "cd " + path + " && magick " + fileName + " -resize " + height + "x" + weight + force + " " + newName + "." + extension;
             String thumbnailCmd = "cd " + path + " && magick " + fileName + " -resize 128x128 thumbnail-" + newName + "." + extension;
             readCmdCommand(cmd);
-            if(thumbnail == 1) {
+            if (thumbnail == 1) {
                 readCmdCommand(thumbnailCmd);
             }
             return "successfully";
-        }catch (Exception ex)
-        {
+        } catch (Exception ex) {
             return ex.getMessage();
         }
     }
 
-    public String selectingImageRegion(MultipartFile file, String newName, int height, int weight, int startPositionX, int startPositionY, String extension) {
+    /**
+     *
+     * @param file
+     * @param newName
+     * @param height
+     * @param weight
+     * @param startPositionX
+     * @param startPositionY
+     * @param extension
+     * @return
+     */
+    public String selectingImageRegion(final MultipartFile file, final String newName, final int height, final int weight, final int startPositionX, final int startPositionY, final String extension) {
         try {
             String path = "C:\\Users\\Administrator\\Desktop";
             String fileName = file.getOriginalFilename();
             String cmd = "cd " + path + " && magick " + fileName + " -crop " + height + "x" + weight + "+" + startPositionX + "+" + startPositionY + " " + newName + "." + extension;
             readCmdCommand(cmd);
             return "successfully";
-        }catch (Exception ex)
-        {
+        } catch (Exception ex) {
             return ex.getMessage();
         }
     }
-    public static String readCmdCommand(String cmd) {
-        try
-        {
+    public static String readCmdCommand(final String cmd) {
+        try {
             ProcessBuilder builder = new ProcessBuilder("cmd", "/c", "\"" +  cmd + "\"");
             builder.redirectErrorStream(true);
             Process process =  builder.start();
             InputStreamReader streamReader = new InputStreamReader(process.getInputStream());
             BufferedReader reader = new BufferedReader(streamReader);
             return "successfully";
-        }
-        catch (IOException ex)
-        {
+        } catch (IOException ex) {
             return ex.getMessage();
         }
     }
-
-    public String grayScale(MultipartFile file, String newName, String extension) {
+    /**
+     *
+     * @param file
+     * @param newName
+     * @param extension
+     * @return
+     */
+    public String grayScale(final MultipartFile file, final String newName, final String extension) {
         try {
             String path = "C:\\Users\\Administrator\\Desktop";
             String fileName = file.getOriginalFilename();
-            String cmd = "cd " + path + " && magick " + fileName + " -colorspace Gray " + newName + "." +extension;
+            String cmd = "cd " + path + " && magick " + fileName + " -colorspace Gray " + newName + "." + extension;
             readCmdCommand(cmd);
             return "successfully";
-        }catch (Exception ex)
-        {
+        } catch (Exception ex) {
             return ex.getMessage();
         }
     }
