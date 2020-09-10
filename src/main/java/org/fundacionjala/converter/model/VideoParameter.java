86ad225d3c9f99f68f6d99d0222a0954dcda7763
maryangela23 <angelamdp16@gmail.com>
2020-09-10 17:33:51 -0400

    extract thumbnail from video

--- src/main/java/org/fundacionjala/converter/model/VideoParameter.java
+++ src/main/java/org/fundacionjala/converter/model/VideoParameter.java
@@ -6,7 +6,10 @@ import java.util.Arrays;
 public enum VideoParameter {
   AUDIO("-vn"),
   COMPRESS("-vcodec", "h264", "-acodec", "mp2", "-i"),
-  GIF("-r", "12", "-i");
+  GIF("-r", "12", "-i"),
+  //params for thumbnail
+  TIME("-ss", "10", "-t", "5", "-i"),
+  PALETTE("-vf", "\"fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse\"", "-loop", "0");
 
   private String[] parameter;
 
