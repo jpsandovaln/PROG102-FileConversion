ca4123660bd2acf4fd9d305ea05df3734662a328
roht0515 <rodrigo051599@gmail.com>
2020-09-11 11:57:45 -0400

    replacing double qoutations mark for the single

--- src/main/resources/static/js/metadata/metadata.js
+++ src/main/resources/static/js/metadata/metadata.js
@@ -1,23 +1,23 @@
 $(document).ready(function () {
-    $( "#form-MT" ).submit(function( event ) {
+    $('#form-MT').submit(function( event ) {
         // Stop form from submitting normally
         event.preventDefault();
         $.ajax({
-            type: "POST",
-            url: "/extractMetadata",
+            type: 'POST',
+            url: '/extractMetadata',
             data: new FormData(this),
             enctype: 'multipart/form-data',
             processData: false,
             contentType: false,
             cache: false,
             success: function (data) {
-                console.log("SUCCESS : ", data);
+                console.log('SUCCESS : ', data);
                 $('#textarea_preview').val(data);
-                $('#path').attr("value", data);
-                $("#submitd").prop('disabled', false);
+                $('#path').attr('value', data);
+                $('#submitd').prop('disabled', false);
             },
             error: function (e) {
-                    console.log("ERROR : ", e);
+                    console.log('ERROR : ', e);
             }
         });
     });
