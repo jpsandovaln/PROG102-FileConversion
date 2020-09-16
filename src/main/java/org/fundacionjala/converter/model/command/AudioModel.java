437b22b3c0020b3ac0682626cb706590aaf29a5b
julia-jpg <julia.escalante@fundacion-jala.org>
2020-09-15 21:08:31 -0400

    modified method excecute, now return list of paths

--- src/main/java/org/fundacionjala/converter/model/command/AudioModel.java
+++ src/main/java/org/fundacionjala/converter/model/command/AudioModel.java
@@ -1,11 +1,10 @@
 package org.fundacionjala.converter.model.command;
+
 import org.fundacionjala.converter.model.configPath.ConfigPath;
 import org.fundacionjala.converter.model.parameter.ModelParameter;
 import org.fundacionjala.converter.model.parameter.multimedia.AudioParameter;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,39 +13,6 @@ import java.util.List;
  * @version 0.1
  */
 public class AudioModel implements ICommand {
-
-    private String buildCommand(final String params, final String source, final String target, final String tool) {
-        return tool + " -y -i "  + source + params  + target;
-    }
-
-    /**
-     * Show all output of audio file conversion
-     * @param params string
-     * @param source string
-     * @param target string
-     * @param tool string
-     * @return path of audio file converted
-     */
-    public String convertAudio(final String params, final String source, final String target, final String tool) {
-        try {
-            String command = buildCommand(params, source, target, tool);
-            ProcessBuilder builder = new ProcessBuilder("cmd", "/c", "\"" + command + "\"");
-            builder.redirectErrorStream(true);
-            Process process = builder.start();
-            process.waitFor();
-            InputStreamReader streamReader = new InputStreamReader(process.getInputStream());
-            BufferedReader reader = new BufferedReader(streamReader);
-            StringBuilder result = new StringBuilder();
-            while (reader.ready()) {
-                result.append((char) reader.read());
-            }
-            System.out.println(result.toString());
-            return target;
-        } catch (java.io.IOException | java.lang.InterruptedException e) {
-            System.out.println(e.getMessage());
-            return "An error occurs while converting file, please try again.";
-        }
-    }
 
     /**
      * create command
@@ -73,9 +39,6 @@ public class AudioModel implements ICommand {
 
     private List createCommandConvert(final List<String> commandsConvert, final ModelParameter modelParameter) {
         commandsConvert.add(modelParameter.getInputFile());
-        String nameFile = ((AudioParameter) modelParameter).getAudioName();
-        String formatFile = ((AudioParameter) modelParameter).getAudioFormat();
-        commandsConvert.add(modelParameter.getOutputFile() + nameFile + formatFile);
 
         if (((AudioParameter) modelParameter).getAudioCodec() != null) {
             commandsConvert.add("-codec:a");
@@ -105,6 +68,9 @@ public class AudioModel implements ICommand {
             commandsConvert.add("-vol");
             commandsConvert.add(((AudioParameter) modelParameter).getAudioVolume());
         }
+        String nameFile = ((AudioParameter) modelParameter).getAudioName();
+        String formatFile = ((AudioParameter) modelParameter).getAudioFormat();
+        commandsConvert.add(modelParameter.getOutputFile() + nameFile + formatFile);
         return commandsConvert;
     }
 }
