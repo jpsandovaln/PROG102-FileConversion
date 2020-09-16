b382181f756d25b0cc786f6d7b6969083df1dda7
Samuel Loza <starsaminf@gmail.com>
2020-09-16 19:46:18 -0400

    Revert "Feature/refactoring js files"

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $( "#form-MT" ).submit(function( event ) {
+ 
+        // Stop form from submitting normally
+        event.preventDefault();
+        console.log("hola");
+
+        $.ajax({
+            type: "POST",
+            url: "/extractMetadata",
+            data: new FormData(this),
+            enctype: 'multipart/form-data',
+            processData: false,
+            contentType: false,
+            cache: false,
+            success: function (data) {
+                console.log("SUCCESS : ", data);
+                $('#textarea_preview').val(data);
+                $('#path').attr("value", data);
+                $("#submitd").prop('disabled', false);
+            },
+            error: function (e) {
+                    console.log("ERROR : ", e);
+            }
+        });
+    
+    });
+});
