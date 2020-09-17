3c1162c62f592fc09d3c9af505447333eabd3ff0
roht0515 <rodrigo051599@gmail.com>
2020-09-17 10:20:50 -0400

    refactor and validation on frontend

---
+++ src/main/resources/static/js/docs/doc.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $('#form-ET').submit(function( event ) {
+        // Stop form from submitting normally
+        event.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: '/extractText',
+            data: new FormData(this),
+            enctype: 'multipart/form-data',
+            processData: false,
+            contentType: false,
+            cache: false,
+            success: function (data) {
+                console.log('SUCCESS : ', data);
+            },
+            error: function (e) {
+                    console.log('ERROR : ', e);
+            }
+        });
+    });
+});
