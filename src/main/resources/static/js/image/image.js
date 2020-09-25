f7878b59daed9af3de823242d516846e89d0fbd0
roht0515 <rodrigo051599@gmail.com>
2020-09-25 11:01:00 -0400

    validation on Image view

--- src/main/resources/static/js/image/image.js
+++ src/main/resources/static/js/image/image.js
@@ -1,26 +1,112 @@
 $(document).ready(function () {
-  $("#image-nav").addClass("btn white black-text waves-effect waves-blue-grey lighten-1");
-    $('#form-image').submit(function( event ) {
-        // Stop form from submitting normally
-        event.preventDefault();
-        $.ajax({
-            type: 'POST',
-            url: '/convertImage',
-            data: new FormData(this),
-            enctype: 'multipart/form-data',
-            processData: false,
-            contentType: false,
-            cache: false,
-            success: function (data) {
-                console.log('SUCCESS : ', data);
-                $('#filenameDownload').attr('value', $('#fileName').val());
-                $('#path').attr('value', data["message"]);
-                $('#submited').prop('disabled', false);
-                console.log(data["message"]);
+    $("#image-nav").addClass("btn white black-text waves-effect waves-blue-grey lighten-1");
+    $("#form-image").validate( {
+        rules: {
+            file: 'required',
+            name: 'required',
+            format: 'required',
+            md5: {
+                required: true,
+                minlength: 32,
             },
-            error: function (e) {
-                console.log('ERROR : ', e);
+            positionX: {
+                required:{
+                depends:function ()
+                    {
+                        if($('#reSize').prop('checked'))
+                        {
+                            return true;
+                        }
+                        return false;
+
+                    }
+                }
+            },
+             positionY: {
+                 required:{
+                 depends:function ()
+                     {
+                         if($('#reSize').prop('checked'))
+                         {
+                             return true;
+                         }
+                         return false;
+                     }
+                 }
+             },
+            height: {
+                required:true,
+                number:true
+            },
+            width: {
+                required:true,
+                number:true
             }
-        });
-    });
+        },
+        messages: {
+            file: 'please insert the file',
+            name: 'please insert the name',
+            format: 'please insert the format',
+            md5: {
+                required: "Please, insert the md5",
+                minlength: "Your md5 code must consist of at least 32 characters",
+            },
+            positionX: 'please insert the channel',
+            positionY: 'please insert if its cut',
+            height: {
+                required:'please, insert the height',
+                number: 'Please, insert just numbers'
+            },
+            width: {
+                required:'please, insert the width',
+                number: 'Please, insert just numbers'
+            }
+        },
+        errorElement: "div",
+        errorPlacement: function ( error, element ) {
+            // Add the `help-block` class to the error element
+            error.addClass("error");
+            element.parent().append(error);
+            element.addClass('invalid');
+            element.addClass('select-validate');
+        },
+        highlight: function ( element, errorClass, validClass ) {
+            $(element).addClass('invalid');
+            $(element).removeClass('valid');
+            $(element).removeClass('select-was-validate');
+            $(element).addClass('select-validate');
+        },
+        unhighlight: function ( element, errorClass, validClass ) {
+            $(element).removeClass('invalid');
+            $(element).addClass('valid');
+            $(element).addClass('select-was-validate');
+            $(element).removeClass('select-validate');
+        },
+        submitHandler: function(form) {
+          $.ajax({
+                   type: 'POST',
+                   url: '/convertImage',
+                   data: new FormData(form),
+                   enctype: 'multipart/form-data',
+                   processData: false,
+                   contentType: false,
+                   cache: false,
+                   success: function (data) {
+                        console.log('SUCCESS : ', data);
+                        $('#filenameDownload').attr('value', $('#fileName').val());
+                        $('#path').attr('value', data["message"]);
+                        $('#submited').prop('disabled', false);
+                        console.log(data["message"]);
+                   },
+                   error: function (e) {
+                       console.log('ERROR : ', e);
+                       Swal.fire({
+                           icon: 'error',
+                           title: e.responseJSON.error,
+                           showConfirmButton: true
+                         })
+                   }
+               });
+        }
+    } );
 });
