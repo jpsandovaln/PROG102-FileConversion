e3bcfda12f38c917d8e3bd666ac9def9419cbe44
maryangela23 <angelamdp16@gmail.com>
2020-09-25 10:52:49 -0400

    validation to md5

--- src/main/resources/static/js/extractText/extractTextValidation.js
+++ src/main/resources/static/js/extractText/extractTextValidation.js
@@ -9,16 +9,18 @@ $("#form-extract").validate({
             required: true
         },
         exportFormat:"required",
+        md5:"required"
     },
     //For custom messages
     messages: {
         name:{
-            required: "Please select a name",
+            required: "Please insert a name",
             minlength: "Enter at least 4 characters"
         },
         file: "Please select a file",
         language: "Please select a language",
-        exportFormat: "Please select a format to export"
+        exportFormat: "Please select a format to export",
+        md5:"Please insert md5"
     },
     errorElement : 'div',
     errorPlacement: function(error, element) {
