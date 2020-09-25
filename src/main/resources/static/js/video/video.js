cedc300f4df3e2dc63e632249b06bae1936f0040
roht0515 <rodrigo051599@gmail.com>
2020-09-25 11:46:30 -0400

    video validations added

--- src/main/resources/static/js/video/video.js
+++ src/main/resources/static/js/video/video.js
@@ -1,27 +1,74 @@
 $(document).ready(function () {
-  $("#video-nav").addClass("btn white black-text waves-effect waves-blue-grey lighten-1");
-    $('#form-video').submit(function( event ) {
-        // Stop form from submitting normally
-        event.preventDefault();
-        $.ajax({
-            type: 'POST',
-            url: '/convertVideo',
-            data: new FormData(this),
-            enctype: 'multipart/form-data',
-            processData: false,
-            contentType: false,
-            cache: false,
-            success: function (data) {
-                console.log('SUCCESS : ', data);
-                $('#textarea_preview').val(data);
-                $('#path').attr('value', data["message"]);
-                $('#submited').prop('disabled', false);
-                console.log(data["message"]);
+    $("#image-nav").addClass("btn white black-text waves-effect waves-blue-grey lighten-1");
+    $("#form-video").validate( {
+        rules: {
+            file: 'required',
+            name: 'required',
+            exportFormat: 'required',
+            md5: {
+                required: true,
+                minlength: 32,
             },
-            error: function (e) {
-                    console.log('ERROR : ', e);
+            videoCodec: 'required',
+            audioCodec: 'required',
+            frames: 'required',
+            sampleRate: 'required',
+            extractGif : {
+                required: false,
             }
-        });
-    });
+        },
+        messages: {
+            file: 'please insert the file',
+            name: 'please insert the name',
+            exportFormat: 'please insert the format',
+            md5: {
+                required: "Please, insert the md5",
+                minlength: "Your md5 code must consist of at least 32 characters",
+            },
+            videoCodec: 'Please, insert the video codec',
+            audioCodec: 'Please, insert the audio codec',
+            frames: 'Please, insert the Frame',
+            sampleRate: 'Please, insert the sample rate',
+        },
+        errorElement: "div",
+        errorPlacement: function ( error, element ) {
+            // Add the `help-block` class to the error element
+            error.addClass("error");
+            element.parent().append(error);
+            element.addClass('invalid');
+            element.addClass('select-validate');
+        },
+        highlight: function ( element, errorClass, validClass ) {
+            $(element).addClass('invalid');
+            $(element).removeClass('valid');
+            $(element).removeClass('select-was-validate');
+            $(element).addClass('select-validate');
+        },
+        unhighlight: function ( element, errorClass, validClass ) {
+            $(element).removeClass('invalid');
+            $(element).addClass('valid');
+            $(element).addClass('select-was-validate');
+            $(element).removeClass('select-validate');
+        },
+        submitHandler: function(form) {
+          $.ajax({
+                   type: 'POST',
+                   url: '/convertVideo',
+                   data: new FormData(form),
+                   enctype: 'multipart/form-data',
+                   processData: false,
+                   contentType: false,
+                   cache: false,
+                   success: function (data) {
+                        console.log('SUCCESS : ', data);
+                        $('#textarea_preview').val(data);
+                        $('#path').attr('value', data);
+                        $('#submitd').prop('disabled', false);
+                   },
+                   error: function (e) {
+                       console.log('ERROR : ', e);
+                   }
+               });
+        }
+    } );
 });
-
