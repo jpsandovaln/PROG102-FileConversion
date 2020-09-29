89d5accd6c2549c6fb5c2e6e6f8ad5edb52b144b
maryangela23 <angelamdp16@gmail.com>
2020-09-29 13:39:55 -0400

    added test to extractTextParameter

---
+++ src/test/java/org/fundacionjala/converter/model/parameter/extractText/ExtractTextParameterTest.java
@@ -0,0 +1,36 @@
+package org.fundacionjala.converter.model.parameter.extractText;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExtractTextParameterTest {
+
+    @Test
+    public void getFileName() {
+    }
+
+    @Test
+    public void setFileName() {
+    }
+
+    @Test
+    public void getLanguage() {
+    }
+
+    @Test
+    public void setLanguage() {
+    }
+
+    @Test
+    public void getFormat() {
+    }
+
+    @Test
+    public void setFormat() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+}
