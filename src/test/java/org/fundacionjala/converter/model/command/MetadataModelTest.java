92bd8c8c8ac0ecc01856a2caa2c1bbea200b6191
eli7890 <eliz.bf.4422@gmail.com>
2020-09-29 21:18:47 -0400

    Unit Test to Metadata

---
+++ src/test/java/org/fundacionjala/converter/model/command/MetadataModelTest.java
@@ -0,0 +1,23 @@
+package org.fundacionjala.converter.model.command;
+
+import org.fundacionjala.converter.executor.Executor;
+import org.fundacionjala.converter.model.parameter.metadata.MetadataParameter;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MetadataModelTest {
+
+    @Test
+    void createCommand() throws InterruptedException, ExecutionException, IOException {
+        MetadataParameter mp = new MetadataParameter("storage\\inputFiles\\aud.mp3", "j", "v", "meta", "abcabcabc");
+        Executor e = new Executor();
+        MetadataModel mm = new MetadataModel();
+        String result = e.executeCommandsList(mm.createCommand(mp)).toString();
+        String expected = "[E:\\Workspace\\PROG102\\Project\\PROG102-FileConversion\\storage\\convertedFiles\\meta.json]";
+        assertEquals(result, expected);
+    }
+}
