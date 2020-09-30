630bd11cb29d307664260144ac1fb35bfdd765d0
Samuel Loza <starsaminf@gmail.com>
2020-09-30 00:55:15 -0400

    Fixed MD5 and change tessData

--- src/main/java/org/fundacionjala/converter/model/command/extractText/ExtractTextModel.java
+++ src/main/java/org/fundacionjala/converter/model/command/extractText/ExtractTextModel.java
@@ -21,7 +21,7 @@ import org.fundacionjala.converter.model.parameter.extractText.ExtractTextParame
  */
 public class ExtractTextModel implements ICommand<ExtractTextParameter> {
     public static final String LANGUAGE_EN = "en";
-
+    public static final String TESSDATA_DIR = "--tessdata-dir";
     public ExtractTextModel() {
     }
 
@@ -44,6 +44,8 @@ public class ExtractTextModel implements ICommand<ExtractTextParameter> {
     private List<String> extractText(final ExtractTextParameter parameter) {
         List<String> command = new ArrayList<String>();
         command.add(ConfigPath.getExtractTextTool());
+        command.add(TESSDATA_DIR);
+        command.add(ConfigPath.getTesstDataDir());
         if (!parameter.getLanguage().equals(LANGUAGE_EN)) {
             command.add(parameter.LANG_COMMAND);
             command.add(parameter.getLanguage());
