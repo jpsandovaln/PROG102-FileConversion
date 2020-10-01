c27a22b93bbbc29e87776f4655afbf9fe2ffb6aa
maryangela23 <angelamdp16@gmail.com>
2020-10-01 11:25:40 -0400

    added exceptions

--- src/main/java/org/fundacionjala/converter/controller/service/FileZipped.java
+++ src/main/java/org/fundacionjala/converter/controller/service/FileZipped.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2020 Fundacion Jala.
+ *
+ * This software is the confidential and proprietary information of Fundacion Jala
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with Fundacion Jala
+ */
 package org.fundacionjala.converter.controller.service;
 
 import org.apache.commons.io.FilenameUtils;
@@ -14,7 +22,7 @@ import java.util.zip.ZipOutputStream;
 
 public class FileZipped {
     private static final int BYTE = 1024;
-    private static final String FORMATZIP = ".zip";
+    private static final String FORMAT_ZIP = ".zip";
     private static final String SLASH = "/";
     private int ex;
     public FileZipped(final int ex) {
@@ -22,20 +30,19 @@ public class FileZipped {
     }
 
     /**
-     *
+     * Compresses the generated files
      * @param modelParameter to get the format attribute that is inside of the modelparameter
      * @param list the list that returns the executor with the path of the files converted
      * @return the path of the zip
      * @throws IOException
      */
     public static String zipper(final ModelParameter modelParameter, final List<String> list) throws IOException {
-        ZipOutputStream os = new ZipOutputStream(new FileOutputStream(modelParameter.getOutputFile() + SLASH + modelParameter.getMd5() + FORMATZIP));
+        ZipOutputStream os = new ZipOutputStream(new FileOutputStream(modelParameter.getOutputFile() + SLASH + modelParameter.getMd5() + FORMAT_ZIP));
         os.setLevel(Deflater.DEFAULT_COMPRESSION);
         os.setMethod(Deflater.DEFLATED);
-        int num = 0;
         for (String file: list) {
-            ZipEntry entrada = new ZipEntry(num + "." + FilenameUtils.getExtension(file));
-            os.putNextEntry(entrada);
+            ZipEntry entry = new ZipEntry(FilenameUtils.getName(file));
+            os.putNextEntry(entry);
             FileInputStream fis = new FileInputStream(file);
             byte[] buffer = new byte[BYTE];
             int leido = 0;
@@ -43,7 +50,6 @@ public class FileZipped {
                 os.write(buffer, 0, leido);
             }
             fis.close();
-            num++;
         }
         os.closeEntry();
         os.close();
@@ -52,6 +58,6 @@ public class FileZipped {
             file = new File(path);
             file.delete();
         }
-        return modelParameter.getOutputFile() + SLASH + modelParameter.getMd5() + FORMATZIP; //modelParameter.getOutputFile() + ".zip";
+        return modelParameter.getOutputFile() + SLASH + modelParameter.getMd5() + FORMAT_ZIP; //modelParameter.getOutputFile() + ".zip";
     }
 }
