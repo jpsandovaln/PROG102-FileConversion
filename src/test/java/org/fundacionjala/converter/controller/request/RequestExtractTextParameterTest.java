eb2eb556161a32037f981cf798cb5691a3bc994c
maryangela23 <angelamdp16@gmail.com>
2020-10-03 12:36:31 -0400

    added test to file

--- src/test/java/org/fundacionjala/converter/controller/request/RequestExtractTextParameterTest.java
+++ src/test/java/org/fundacionjala/converter/controller/request/RequestExtractTextParameterTest.java
@@ -3,14 +3,19 @@ package org.fundacionjala.converter.controller.request;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.multipart.MultipartFile;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.fundacionjala.converter.Application;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-
 import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 class RequestExtractTextParameterTest {
+    @Autowired
+    private Application application;
+
     RequestExtractTextParameter request = new RequestExtractTextParameter();
     @Test
     void getLanguage() {
@@ -94,13 +99,37 @@ class RequestExtractTextParameterTest {
         );
     }
     @Test
-    public void md5ValidationTest() {
+    public void emptyFileNameValidationTest() {
+        File file = new File(" ");
         Throwable exception = Assertions.assertThrows(
                 Exception.class, () -> {
                     request.setExportFormat(".txt");
-                    request.setMd5("ffa5f5433efe74bc99530e84798b2ffd");
+                    request.setFile((MultipartFile) file);
                     request.validate();
                 }
         );
+        System.out.println(exception.getMessage());
+    }
+    @Test
+    public void md5ValidationTest() {
+        Throwable exception = Assertions.assertThrows(
+                Exception.class, () -> {
+                    Resource fileResource = new ClassPathResource(
+                            "storage/inputFiles/imagen7.jpg");
+                    //assertNotNull(fileResource);
+                    MockMultipartFile multipartFile = new MockMultipartFile(
+                            "attachments",fileResource.getFilename(),
+                            MediaType.MULTIPART_FORM_DATA_VALUE,
+                            fileResource.getInputStream());
+                    request.setExportFormat(".txt");
+                    request.setLanguage("spa");
+                    request.setFile(multipartFile);
+                    request.setMd5("ffa5f5433efe74bc99530e84798b2ffd");
+                    request.validate();
+
+                }
+
+        );
+        System.out.println(exception.getMessage());
     }
 }
