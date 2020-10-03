ef6c56393c403bb46b427a8ff481dc8dca13ade9
julia-jpg <julia.escalante@fundacion-jala.org>
2020-10-02 21:17:57 -0400

    added exceptions for audioParameters

--- src/test/java/org/fundacionjala/converter/model/parameter/multimedia/AudioParameterTest.java
+++ src/test/java/org/fundacionjala/converter/model/parameter/multimedia/AudioParameterTest.java
@@ -6,18 +6,5 @@ import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertEquals;
 
 class AudioParameterTest {
-    @Test
-    public void testCreateCommandReturnsEmptyListIfParameterIsEmpty() {
-        AudioModel audioModel = new AudioModel();
-        AudioParameter audioParameter = new AudioParameter();
-        int result = audioModel.createCommand(audioParameter).size();
-        assertEquals(0, result);
-    }
-    @Test
-    public void testCreateCommandReturnsEmptyListIfParameterIsNull()  {
-        AudioModel audioModel = new AudioModel();
-        int result = audioModel.createCommand(null).size();
-        assertEquals(0, result);
-    }
 
 }
