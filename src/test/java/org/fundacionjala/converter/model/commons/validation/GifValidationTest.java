8e1aebf1ac3063fff4409302125fc882c5e750ed
laura helen montano toro <hlaura.toro@gmail.com>
2020-10-05 03:08:27 -0400

    Added unit test Gif Validation

--- src/test/java/org/fundacionjala/converter/model/commons/validation/GifValidationTest.java
+++ src/test/java/org/fundacionjala/converter/model/commons/validation/GifValidationTest.java
@@ -1,6 +1,12 @@
 package org.fundacionjala.converter.model.commons.validation;
 
+import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.fundacionjala.converter.model.commons.exception.InvalidDataException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class GifValidationTest {
@@ -25,6 +31,64 @@ public class GifValidationTest {
     }
     @Test
     public void testValidateThrowsExceptionWhenStartIsGreaterThanDuration() {
-        
+        GifValidation gValidation = new GifValidation();
+        gValidation.setDuration(300);
+        try {
+            gifValidation.setStart(350);
+            gifValidation.validate();
+        }
+        catch (InvalidDataException e){
+            assertThat(e.getMessage(), is("Invalid arguments to convert a Gif"));
+        }
+    }
+    @Test
+    public void testValidateThrowsExceptionWhenTimeIsGreaterThanStart() {
+        GifValidation gValidation = new GifValidation(duration, start, "400");
+        try {
+            gValidation.validate();
+        }
+        catch (InvalidDataException e){
+            assertThat(e.getMessage(), is("Invalid arguments to convert a Gif"));
+        }
+    }
+    @Test
+    public void testValidateThrowsExceptionWhenStartIsEqualsThanDuration() {
+        GifValidation gValidation = new GifValidation(duration, duration, "5");
+        try {
+            gValidation.validate();
+        }
+        catch (InvalidDataException e){
+            assertThat(e.getMessage(), is("Invalid arguments to convert a Gif"));
+        }
+    }
+    @Test
+    public void testValidateThrowsExceptionWhenStartPlusTimeIsGreaterThanDuration() {
+        GifValidation gValidation = new GifValidation(duration, start, "500");
+        try {
+            gValidation.validate();
+        }
+        catch (InvalidDataException e){
+            assertThat(e.getMessage(), is("Invalid arguments to convert a Gif"));
+        }
+    }
+    @Test
+    public void testValidateThrowsException() {
+        GifValidation gValidation = new GifValidation(duration, start, "500");
+        try {
+            gValidation.validate();
+       //     fail();
+        }
+        catch (InvalidDataException e){
+            assertThat(e.getMessage(), is("Invalid arguments to convert a Gif"));
+        }
+      //  GifValidation gValidation = new GifValidation(start, start, "500");
+      //  fail();
+     /*   Throwable exception = Assertions.assertThrows(
+            InvalidDataException.class, () -> {
+          //      e.get
+            //    gValidation.validate();
+            GifValidation gValidation = new GifValidation(start, start, "500");
+            }
+        );*/
     }
 }
